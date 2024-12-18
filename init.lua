10c847edb8ad74ebd6bc12e9aded36f0393943a7
Paweł Wójcik <pawel.wojcik5@gmail.com>
2024-12-17 18:34:26 -0800

    Add quickfix and terminal config.

--- init.lua
+++ init.lua
@@ -1,8 +1,26 @@
 vim.keymap.set("n", "<space>e", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space><space>e", "<cmd>.lua<CR>")
 vim.keymap.set("v", "<space>e", ":lua<CR>")
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
-vim.opt.shiftwidth = 4
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
+  callback = function()
+    vim.opt.number = true
+    vim.opt.relativenumber = true
+  end,
+})
+
+
+-- vim.keymap.set('t', '<Esc>', [[<C-\><C-N>]])
+vim.keymap.set('n', '<leader>st', function()
+  vim.cmd.new()
+  vim.cmd.term()
+  vim.cmd.wincmd("K")
+  vim.api.nvim_win_set_height(0, 8)
+end)
+
 
 require("config.lazy")
 require("config.set")
