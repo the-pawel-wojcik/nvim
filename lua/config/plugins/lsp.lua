8f66352f7731e7924817dc4792c715d7fcd748dd
Paweł Wójcik <pawel.wojcik5@gmail.com>
2024-12-18 22:29:56 -0800

    Add autocomplete with blink

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -2,19 +2,24 @@ return {
   {
     "neovim/nvim-lspconfig",
     dependencies = {
-      "folke/lazydev.nvim",
-      ft = "lua", -- only load on lua files
-      opts = {
-        library = {
-          -- See the configuration section for more details
-          -- Load luvit types when the `vim.uv` word is found
-          { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      "saghen/blink.nvim",
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
         },
       },
     },
     config = function()
+      local capabilities = require('blink.cmp').get_lsp_capabilities()
+      require("lspconfig").lua_ls.setup { capabilities = capabilities }
+
       local opts = { remap = false }
-      require("lspconfig").lua_ls.setup {}
       vim.keymap.set({ "n", "v", "x" }, "<space>f", function()
         vim.lsp.buf.format()
       end)
