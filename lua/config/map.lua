3544f367b8946590db8bc87f9e40a5a9acff90d7
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-01-07 15:23:11 -0800

    Add favourite maps

--- lua/config/map.lua
+++ lua/config/map.lua
@@ -1,3 +1,17 @@
 vim.keymap.set("n", "<leader>e", "<cmd>source %<CR>")
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<leader>date",
+  "<cmd>r!date +'\\%a, \\%b, \\%dth, \\%Y' <bar> sed 's/\\([023]1\\)th/\\1st/' <bar> sed 's/\\([02]2\\)th/\\1nd/' <bar> sed 's/\\([02]3\\)th/\\1rd/' <CR>")
+-- Open the file manager in the directory containing current file
+vim.keymap.set("n", "<leader>w", "<cmd>!xdg-open %:p:h<CR>")
+
+-- just paste it ®
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+-- paste over with the yank register insted of the unnamed register
+vim.keymap.set("x", "<leader>p", [["0P]])
+vim.keymap.set("x", "<leader>P", [["0P]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["+d]])
