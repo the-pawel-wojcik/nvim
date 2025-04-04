dd3188685f1760d09eacfb4d2cd933d3457bdc24
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-04-04 14:52:54 -0400

    Add cmake to treesitter and fix disable

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -7,6 +7,7 @@ return {
         ensure_installed = {
           "cpp", "c", "lua", "vim", "vimdoc",
           "python", "bash",
+          "cmake",
           "query",
           "json",
           "markdown", "markdown_inline"
@@ -16,12 +17,20 @@ return {
 
         highlight = {
           enable = true,
-          disable = { "latex" },
-          -- Or use a function for more flexibility, e.g. to disable slow
-          -- treesitter highlight for large files
+          -- Disable selected languages
+          -- disable = { "latex" },
+          -- Or use a function for more flexibility, e.g.,
           disable = function(lang, buf)
+            -- Let vimtex handle highlights of .tex files
+            if lang == "tex" then
+              return true
+            end
+            -- disable slow treesitter highlight for large files
             local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            local ok, stats = pcall(
+              vim.loop.fs_stat,
+              vim.api.nvim_buf_get_name(buf)
+            )
             if ok and stats and stats.size > max_filesize then
               return true
             end
