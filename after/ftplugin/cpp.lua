c04a73b12549fa5ba0ebb7e7dbd805c4a9bdc49e
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-04-21 15:27:21 -0400

    Stop wrapping c++

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,5 +1,8 @@
-vim.keymap.set("n", "<leader>e", "<cmd>!make %:r && ./%:r<CR>")
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+vim.keymap.set("n", "<leader>e", "<cmd>w<CR><cmd>!make %:r && ./%:r<CR>")
+
+local set = vim.opt_local
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
+set.expandtab = true
+set.wrap = false
