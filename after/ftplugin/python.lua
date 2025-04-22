8017fc6295c1039b8162433cf7fd36f7f93f69d4
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-04-22 09:01:12 -0400

    python: save before running doctest

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,3 +1,3 @@
 -- execute this file
 vim.keymap.set("n", "<leader>e", "<cmd>w<CR><cmd>!python %<CR>")
-vim.keymap.set("n", "<leader>td", "<cmd>!python -m doctest -v %<CR>")
+vim.keymap.set("n", "<leader>td", "<cmd>w<CR><cmd>!python -m doctest -v %<CR>")
