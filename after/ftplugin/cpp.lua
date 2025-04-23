5a967be16cb7fdd83d0728de3bb25faa9bf6353d
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-04-23 15:42:07 -0400

    c++ maps for working with cmake

--- after/ftplugin/cpp.lua
+++ after/ftplugin/cpp.lua
@@ -1,4 +1,13 @@
 vim.keymap.set("n", "<leader>e", "<cmd>w<CR><cmd>!make %:r && ./%:r<CR>")
+-- cmake execute
+vim.keymap.set("n", "<leader>ce", "<cmd>!cd ..; make run RUN=%:t:r<CR>")
+-- cmake build & execute
+vim.keymap.set(
+  "n", "<leader>cb",
+  "<cmd>w<CR><cmd>!cd ..; make compile; make run RUN=%:t:r<CR>"
+)
+-- cmake clean, configure, build, & execute
+vim.keymap.set("n", "<leader>cc", "<cmd>w<CR><cmd>!cd ..; make clean; make configure; make compile; make run RUN=%:t:r<CR>")
 
 local set = vim.opt_local
 set.tabstop = 2
