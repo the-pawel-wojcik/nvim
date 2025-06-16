288c6cce8270bcc1768b0da0892b9e6b8ae8a009
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-06-16 11:20:32 -0400

    python: <leader>tsp does pytest -s

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,7 +1,8 @@
 -- execute this file
 vim.keymap.set("n", "<leader>e", "<cmd>w<CR><cmd>!python %<CR>")
 vim.keymap.set("n", "<leader>td", "<cmd>w<CR><cmd>!python -m doctest -v %<CR>")
-vim.keymap.set("n", "<leader>pt", "<cmd>w<CR><cmd>!pytest %<CR>")
+vim.keymap.set("n", "<leader>tp", "<cmd>w<CR><cmd>!pytest %<CR>")
+vim.keymap.set("n", "<leader>tsp", "<cmd>w<CR><cmd>!pytest -s %<CR>")
 
 local set = vim.opt_local
 set.textwidth=80
