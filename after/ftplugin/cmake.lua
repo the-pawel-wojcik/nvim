e0bbfb5134138fbe20537c7a41314cf3c54a0b89
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-06-30 09:36:47 -0400

    cmake: <leader>c creates build; <leader>b builds

--- after/ftplugin/cmake.lua
+++ after/ftplugin/cmake.lua
@@ -1,4 +1,10 @@
--- build
+vim.keymap.set(
+  "n",
+  "<leader>e",
+  "<cmd>w<CR><cmd>!cmake -P %<CR>"
+)
+
+-- create build system
 -- -S argument is the directory hosting the currently opened CMakeLists.txt
 -- -B build is created one level up
 -- Example: if editing project/src/CMakeLists.txt
@@ -6,6 +12,13 @@
 -- -B would get project/build
 vim.keymap.set(
   "n",
-  "<leader>e",
+  "<leader>c",
   "<cmd>w<CR><cmd>!cmake -S %:p:h -B %:p:h:h/build<CR>"
+)
+
+-- build project
+vim.keymap.set(
+  "n",
+  "<leader>b",
+  "<cmd>w<CR><cmd>!cmake --build %:p:h:h/build<CR>"
 )
